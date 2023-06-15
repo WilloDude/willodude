2b8b0b16716e4d297505413b3cdd65ca9750a5c0
William M <billymillena@gmail.com>
2023-06-15 22:29:10 +1000

    d
    d

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-﻿/* -----------------------------------------------------------------------------------
-   Developed by the Applications Prototype Lab
-   (c) 2015 Esri | http://www.esri.com/legal/software-license  
------------------------------------------------------------------------------------ */
-
-require([
+﻿require([
     'esri/Map',
     'esri/Camera',
     'esri/views/MapView',
@@ -12,8 +7,7 @@ require([
     'esri/geometry/Point',
     'esri/geometry/support/webMercatorUtils',
     'dojo/domReady!'
-],
-function (
+], function (
     Map,
     Camera,
     MapView,
@@ -21,16 +15,15 @@ function (
     Viewpoint,
     Point,
     webMercatorUtils
-    ) {
+) {
     $(document).ready(function () {
-        // Enforce strict mode
         'use strict';
 
         // Plane flight details
         var lastTime = null;
-        var speed = 0; // m/s, updated by throttle
-        var heading = 0; // updated by joystick
-        var altitude = 3000; // m, updated by joystick
+        var speed = 200; // m/s
+        var heading = 0;
+        var altitude = 3000;
         var location = new Point({
             x: 779232,
             y: 5780430,
@@ -84,8 +77,6 @@ function (
         viewLeft.ui.components = [];
         viewRight.ui.components = [];
 
-        window.requestAnimationFrame(draw);
-
         function draw(time) {
             if (map.loaded && lastTime !== null) {
                 var t = time - lastTime; // ms
@@ -125,75 +116,41 @@ function (
                 $('#dial-location-y').html(ConvertDDToDMS(geographic.y, false));
             }
             lastTime = time;
-            window.requestAnimationFrame(draw);
+            requestAnimationFrame(draw);
         }
 
-        // Function to handle joystick input
-        function handleJoystickInput(joystickX, joystickY) {
-            // Convert joystick input to desired values
-            heading = joystickX * 360; // Assumes joystickX ranges from -1 to 1, convert to degrees (0-360)
-            altitude += joystickY * 100; // Assumes joystickY ranges from -1 to 1, convert to meters
+        window.requestAnimationFrame(draw);
 
-            // Clamp altitude within valid range
-            if (altitude < 0) {
-                altitude = 0;
+        // Update speed based on throttle control
+        $('#throttle-control').on('input', function () {
+            speed = parseInt($(this).val());
+        });
+
+        // Update heading based on joystick control
+        $('#joystick-control').on('input', function () {
+            heading = parseInt($(this).val());
+        });
+
+        // Update altitude based on joystick control
+        $('#joystick-altitude-control').on('input', function () {
+            altitude = parseInt($(this).val());
+        });
+
+        function ConvertDDToDMS(d, lng) {
+            var dir = d < 0 ? (lng ? 'W' : 'S') : (lng ? 'E' : 'N');
+            var deg = 0 | (d < 0 ? d = -d : d);
+            var min = 0 | (d % 1 * 60);
+            var sec = (0 | (d * 60 % 1 * 60));
+            return deg + '° ' + format.format('02d')(min) + '\' ' + format.format('02d')(sec) + '" ' + dir;
+        }
+
+        String.prototype.format = function () {
+            var s = this;
+            var i = arguments.length;
+            while (i--) {
+                s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
             }
-
-            // Update the altitude display element if needed
-            $('#dial-altitude').html(format.format(',')(altitude) + ' m');
-        }
-
-        // Function to handle throttle input
-        function handleThrottleInput(throttleValue) {
-            // Convert throttle input to desired speed range
-            speed = throttleValue * 1000; // Assumes throttleValue ranges from 0 to 1, convert to meters per second
-
-            // Update the speed display element if needed
-            $('#dial-speed').html(format.format(',')(speed) + ' m/s');
-        }
-
-        // Simulated joystick and throttle input update loop
-        setInterval(function() {
-            // Simulated values for joystick and throttle inputs (replace with actual input reading code)
-            var joystickX = 0.5; // Example: joystick X-axis value (ranging from -1 to 1)
-            var joystickY = 0.8; // Example: joystick Y-axis value (ranging from -1 to 1)
-            var throttleValue = 0.7; // Example: throttle value (ranging from 0 to 1)
-
-            handleJoystickInput(joystickX, joystickY);
-            handleThrottleInput(throttleValue);
-        }, 100); // Update every 100 milliseconds (adjust this according to your needs)
-
-        // Rest of the code...
-        // ...
+            return s;
+        };
     });
 });
-
-// --------------------------------------------------------------------------------------------
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-// RequestAnimationFrame polyfill by Erik Möller
-// Fixes from Paul Irish and Tino Zijdel
-// --------------------------------------------------------------------------------------------
-(function () {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
-                                      window[vendors[x] + 'CancelRequestAnimationFrame'];
-    }
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () { callback(currTime + timeToCall); }, timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
-    if (!window.cancelAnimationFrame) {
-        window.cancelAnimationFrame = function (id) {
-            clearTimeout(id);
-        };
-    }
-}());
